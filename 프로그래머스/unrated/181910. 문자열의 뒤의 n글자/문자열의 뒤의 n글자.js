7c8b8b9e9caa596319f5ab499b810f6fb3791818
Yoo Jiwon <jiwonyoo66@gmail.com>
2023-10-11 21:12:26 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -1,5 +1,3 @@
 function solution(my_string, n) {
-    var answer = '';
-    answer = my_string.slice(my_string.length - n, my_string.length)
-    return answer;
+    return my_string.slice(my_string.length - n);
 }
