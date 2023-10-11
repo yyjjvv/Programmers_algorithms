ef9be1299f00d0517e8b757fe34a9135f3fc0572
Yoo Jiwon <jiwonyoo66@gmail.com>
2023-10-11 23:18:23 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,9 +1,14 @@
 function solution(age) {
-    const ageStr = String(age);
-    const alphabet = "abcdefghij";
-    var answer = "";
-    for (let i = 0; i < ageStr.length; i++) {
-        answer += alphabet.charAt(Number(ageStr.charAt(i)));
-    }
-    return answer;
+    return age
+    .toString()
+    .split("")
+    .map((v) => "abcdefghij"[v])
+    .join("");
+    // const ageStr = String(age);
+    // const alphabet = "abcdefghij";
+    // var answer = "";
+    // for (let i = 0; i < ageStr.length; i++) {
+    //     answer += alphabet.charAt(Number(ageStr.charAt(i)));
+    // }
+    // return answer;
 }
