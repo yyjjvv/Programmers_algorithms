e84fb7aa956caca6a60020a4377330e33c673703
Yoo Jiwon <jiwonyoo66@gmail.com>
2023-10-13 23:50:18 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,8 +1,8 @@
 function solution(num_list, n) {
     const answer = [];
     
-    for(let i = 0; i < num_list.length / n; i++) {
-        answer.push(num_list.slice(i * n, i * n + n))
+    while(num_list.length) {
+        answer.push(num_list.splice(0, n));
     }
     
     return answer;
