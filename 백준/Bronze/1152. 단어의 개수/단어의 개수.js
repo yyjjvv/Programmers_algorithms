ad7952e92313c9711079ed7d5c25b9d25a4515eb
Yoo Jiwon <jiwonyoo66@gmail.com>
2023-11-04 01:39:15 +0900

    [Bronze II] Title: 단어의 개수, Time: 180 ms, Memory: 23036 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let data = input[0].trim().split(" ");
+if (data == "") {
+    console.log(0);
+} else {
+    console.log(data.length);
+}
