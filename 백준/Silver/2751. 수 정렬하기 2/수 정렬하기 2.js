82ccf74030e9d36060025c02a5d6d8d56387dab8
Yoo Jiwon <jiwonyoo66@gmail.com>
2023-11-06 21:48:09 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1440 ms, Memory: 215540 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i++) {
+  arr.push(Number(input[i]));
+}
+// 오름차순 정렬 수행
+arr.sort(function(a, b) { return a - b;
+})
+let answer = "";
+for (let i = 0; i < arr.length; i++) {
+  answer += arr[i] + "\n";
+}
+console.log(answer);
